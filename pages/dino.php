3240d87779421f7dc350617f45040580ab18972e
Dashboard Admin <admin@dashboard.com>
2026-03-23 21:58:45 +0700

    Fix iframe src path for Dino game

--- pages/dino.php
+++ pages/dino.php
@@ -4,6 +4,6 @@
 <div class="g-card p-6">
   <h2 class="text-xl font-semibold mb-4 text-center">Dino Game</h2>
   <div class="text-center">
-    <iframe src="../tools/dino/game-dino.html" width="100%" height="400" frameborder="0" class="rounded-lg"></iframe>
+    <iframe src="/dashboard/tools/dino/game-dino.html" width="100%" height="400" frameborder="0" class="rounded-lg"></iframe>
   </div>
 </div>
